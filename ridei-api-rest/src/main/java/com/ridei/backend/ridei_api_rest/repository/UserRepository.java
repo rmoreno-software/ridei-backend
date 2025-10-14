1eed9af2de7968363ff5b79d9c4c99ebb47ff04e
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-10-14 08:04:28 +0200

    eliminem projecte per tornar a començar

--- ridei-api-rest/src/main/java/com/ridei/backend/ridei_api_rest/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ridei.backend.ridei_api_rest.repository;
-
-import com.ridei.backend.ridei_api_rest.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
