f0e88b402dd0e6226b82e32990a858444a1c1d41
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2026-01-15 10:08:18 +0100

    Creació Projecte Spring buit

---
+++ ridei-backend/src/main/java/com/ridei/ridei_backend/RideiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ridei.ridei_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideiBackendApplication.class, args);
+	}
+
+}
