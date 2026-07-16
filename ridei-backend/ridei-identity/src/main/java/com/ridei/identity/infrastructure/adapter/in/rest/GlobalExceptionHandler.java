ddeb8ea5eccf85e7d69dc37dc5ff27ed6cb935fc
Roger Moreno González <roger.moreno@terrassa.cat>
2026-07-16 17:54:25 +0200

    Correcció import

--- ridei-backend/ridei-identity/src/main/java/com/ridei/identity/infrastructure/adapter/in/rest/GlobalExceptionHandler.java
+++ ridei-backend/ridei-identity/src/main/java/com/ridei/identity/infrastructure/adapter/in/rest/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
